12e990db5b3c63d0234285f97404d42322be4ab3
Ndik <hendik.suwoto@gmail.com>
2023-06-29 11:49:18 +0700

    change slider, add icon

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    autoHideMenuBar: true,
+    icon: 'app.ico',
   })
-
   win.loadFile('index.html')
 }
 
