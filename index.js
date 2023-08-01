298e85283d926b831a88755a9d93b8817b6b00e9
IRedDragonICY <hendik.suwoto@gmail.com>
2023-08-01 23:46:39 +0700

    add background menu and game, add parallex effect, move script in index.html into index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ let splash;
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     autoHideMenuBar: true,
     icon: "app.ico",
     show: false, 
+    fullscreen: true,
+    resizable: false,
     webPreferences: {
       preload: path.join(__dirname, "lib/js/preload.js"),
     },
